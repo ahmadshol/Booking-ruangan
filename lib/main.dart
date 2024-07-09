e61af44d9a57cc3a5b0bfc28653fb69f4cbe5214
ahmadshol <muhammadsholahudin96@gmail.com>
2024-07-10 02:29:58 +0700

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // main.dart
 
 import 'package:Pemesanan_Ruang/splash_screen.dart';
-import 'package:Pemesanan_Ruang/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
